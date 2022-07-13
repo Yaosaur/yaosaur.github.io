c0999fffac0d38e0cdeda931cddd6546ece30c12
Yao Jiang <yaojiang7595@gmail.com>
2022-07-13 15:46:37 -0400

    Added birds to city, polished car placement and movement

--- script.js
+++ script.js
@@ -42,8 +42,6 @@ let iceSources = [
   "images/icecave/icicles/icy4.jpg",
   "images/icecave/icicles/icy5.jpg",
   "images/icecave/icicles/icy6.jpg",
-  "images/icecave/icicles/icy7.jpg",
-  "images/icecave/icicles/icy8.jpg",
 ];
 let forestSources = ["images/forest/butterflyswarm.png"];
 let citySources = [
@@ -52,7 +50,9 @@ let citySources = [
   "images/city/car3.png",
   "images/city/car3.png",
 ];
+let citySourcesBirds = ["images/city/pigeon1.png"];
 let images = [];
+let imagesSub = [];
 
 canvas.addEventListener("mousemove", (event) => {
   player.x = event.offsetX;
@@ -81,9 +81,46 @@ function init(
       image.dy = randomNum(dyMin, dyMax);
       image.src = sources[ranArrayIndex];
       image.onload = () => {
-        context.drawImage(image, image.xCord, image.yCord, 100, 100);
+        context.drawImage(image, image.xCord, image.yCord);
       };
       images.push(image);
+    }
+  }
+}
+
+function init2(
+  sources,
+  desiredParticles,
+  spawnWidthMin,
+  spawnWidthMax,
+  spawnHeightMin,
+  spawnHeightMax,
+  dxMin,
+  dxMax,
+  dyMin,
+  dyMax,
+  imageWidth,
+  imageHeight
+) {
+  while (imagesSub.length < desiredParticles) {
+    for (let i = 0; i < desiredParticles; i++) {
+      let ranArrayIndex = randomNum(0, sources.length - 1);
+      let image = new Image();
+      image.xCord = randomNum(spawnWidthMin, spawnWidthMax);
+      image.yCord = randomNum(spawnHeightMin, spawnHeightMax);
+      image.dx = randomNum(dxMin, dxMax);
+      image.dy = randomNum(dyMin, dyMax);
+      image.src = sources[ranArrayIndex];
+      image.onload = () => {
+        context.drawImage(
+          image,
+          image.xCord,
+          image.yCord,
+          imageWidth,
+          imageHeight
+        );
+      };
+      imagesSub.push(image);
     }
   }
 }
@@ -155,14 +192,39 @@ function moveCars() {
       endGame();
     }
     if (image.xCord < 0) {
-      image.xCord = randomNum(canvas.width - 100, canvas.width - 100);
-      setTimeout(
-        context.drawImage(image, image.xCord, image.yCord, 100, 100),
-        randomNum(5000, 10000)
-      );
+      image.dx = randomNum(3, 7);
+      image.xCord = randomNum(canvas.width - 150, canvas.width - 200);
+      let ranArrayIndex = randomNum(0, citySources.length - 1);
+      image.src = citySources[ranArrayIndex];
     } else {
-      context.drawImage(image, image.xCord, image.yCord, 100, 100);
+      context.drawImage(image, image.xCord, image.yCord, 125, 70);
     }
+  }
+}
+
+function moveBirds() {
+  for (image of imagesSub) {
+    let ranTop = randomNum(10, 30);
+    let ranBottom = randomNum(175, 200);
+    image.xCord += image.dx;
+    image.yCord += image.dy;
+    if (image.yCord < ranTop || image.yCord > ranBottom) {
+      image.dy = -image.dy;
+    }
+    if (image.xCord < 0) {
+      image.dx = randomNum(-3, -10);
+      image.xCord = randomNum(canvas.width - 150, canvas.width - 200);
+      image.yCord = randomNum(50, 150);
+    }
+    if (
+      player.x + player.radius > image.xCord &&
+      player.x - player.radius < image.xCord + 100 &&
+      player.y + player.radius > image.yCord &&
+      player.y - player.radius < image.yCord + 100
+    ) {
+      endGame();
+    }
+    context.drawImage(image, image.xCord, image.yCord, 50, 50);
   }
 }
 
@@ -186,41 +248,69 @@ function animateCity() {
   context.clearRect(0, 0, canvas.width, canvas.height);
   player.draw();
   moveCars();
+  moveBirds();
 }
 
-init(iceSources, 9, 0, canvas.width - 100, 0, 200, 0, 0, 3, 5);
-animateIce();
+// init(iceSources, 9, 0, canvas.width - 100, 0, 200, 0, 0, 3, 5);
+// animateIce();
 
-setTimeout(function () {
-  cancelAnimationFrame(animationID);
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  body.style.backgroundImage = "url('images/forest/forest.jpg')";
-  while (images.length > 0) {
-    images.pop();
-  }
-  init(forestSources, 10, 0, canvas.width - 100, 0, 200, 3, 5, 3, 5);
-  animateForest();
-}, randomNum(5000, 10000));
+// setTimeout(function () {
+//   cancelAnimationFrame(animationID);
+//   context.clearRect(0, 0, canvas.width, canvas.height);
+//   body.style.backgroundImage = "url('images/forest/forest.jpg')";
+//   while (images.length > 0) {
+//     images.pop();
+//   }
+//   init(forestSources, 10, 0, canvas.width - 100, 0, 200, 3, 5, 3, 5);
+//   animateForest();
+// }, randomNum(5000, 10000));
 
-setTimeout(function () {
-  cancelAnimationFrame(animationID);
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  body.style.backgroundImage = "url('images/city/streets.jpg')";
-  while (images.length > 0) {
-    images.pop();
-  }
-  init(
-    citySources,
-    3,
-    canvas.width - 100,
-    canvas.width - 100,
-    300,
-    canvas.height - 100,
-    3,
-    5,
-    0,
-    0
-  );
-  animateCity();
-}, randomNum(15000, 20000));
+// setTimeout(function () {
+//   cancelAnimationFrame(animationID);
+//   context.clearRect(0, 0, canvas.width, canvas.height);
+//   body.style.backgroundImage = "url('images/city/streets.jpg')";
+//   while (images.length > 0) {
+//     images.pop();
+//   }
+//   init(
+//     citySources,
+//     3,
+//     canvas.width - 100,
+//     canvas.width - 100,
+//     300,
+//     canvas.height - 100,
+//     3,
+//     5,
+//     0,
+//     0
+//   );
+//   animateCity();
+// }, randomNum(15000, 20000));
+init(
+  citySources,
+  3,
+  canvas.width - 150,
+  canvas.width - 200,
+  canvas.height * 0.6,
+  canvas.height * 0.65,
+  3,
+  7,
+  0,
+  0
+);
+init2(citySourcesBirds, 4, 0, canvas.width - 100, 50, 150, -3, -10, 1, 1);
+
+animateCity();
 //Notes for self
+
+// context.beginPath();
+// context.moveTo(0, 0);
+// context.lineTo(100, canvas.height * 0.68);
+// context.stroke();
+
+// function randomMovement(move) {
+//   let plusOrMinus = Math.random() < 0.5 ? -1 : 1;
+//   return move * plusOrMinus;
+// }
+
+//Modularize image generation
